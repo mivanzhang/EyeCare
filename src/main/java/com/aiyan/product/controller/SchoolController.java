44d6fab4a5c9d3e2f0717ca2a4be4f1bbca73c2a
zhangmeng05 <zhangmeng05@kuaishou.com>
2018-12-23 16:50:12 +0800

    增加二维码

--- src/main/java/com/aiyan/product/controller/SchoolController.java
+++ src/main/java/com/aiyan/product/controller/SchoolController.java
@@ -9,6 +9,13 @@ import com.aiyan.product.jpa.DoctorRepository;
 import com.aiyan.product.jpa.SchoolRepository;
 import com.aiyan.product.jpa.StudentRepository;
 import com.aiyan.product.jpa.UserRepository;
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.aliyuncs.profile.IClientProfile;
 import org.apache.tomcat.util.bcel.Const;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,9 +27,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Controller
 public class SchoolController {
@@ -120,15 +125,85 @@ public class SchoolController {
         return "school/school_register_step2";
     }
 
+
+    //产品名称:云通信短信API产品,开发者无需替换
+    static final String product = "Dysmsapi";
+    //产品域名,开发者无需替换
+    static final String domain = "dysmsapi.aliyuncs.com";
+
+    static final String accessKeyId = "LTAIIR6kAYMR0k8z";
+    static final String accessKeySecret = "UtOJXJMSWyAcTSdsodEYhKoITNV0XC";
+
+
     public static boolean sendCode(String phoneNumber) {
 
+        try {
+            sendSms(phoneNumber);
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
         return true;
+    }
+
+
+    public static SendSmsResponse sendSms(String phoneNumber) throws ClientException {
+
+        //可自助调整超时时间
+        System.setProperty("sun.net.client.defaultConnectTimeout", "10000");
+        System.setProperty("sun.net.client.defaultReadTimeout", "10000");
+
+        //初始化acsClient,暂不支持region化
+        IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId, accessKeySecret);
+        DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", product, domain);
+        IAcsClient acsClient = new DefaultAcsClient(profile);
+
+        //组装请求对象-具体描述见控制台-文档部分内容
+        SendSmsRequest request = new SendSmsRequest();
+        //必填:待发送手机号
+        request.setPhoneNumbers(phoneNumber);
+        //必填:短信签名-可在短信控制台中找到
+        request.setSignName("爱眼课堂");
+        //必填:短信模板-可在短信控制台中找到
+        request.setTemplateCode("SMS_153331774");
+        //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
+        request.setTemplateParam("{\"code\":" + getRandomNumCode(phoneNumber, 4) + "}");
+
+        //选填-上行短信扩展码(无特殊需求用户请忽略此字段)
+        //request.setSmsUpExtendCode("90997");
+
+        //可选:outId为提供给业务方扩展字段,最终在短信回执消息中将此值带回给调用者
+        request.setOutId("yourOutId");
+
+        //hint 此处可能会抛出异常，注意catch
+        SendSmsResponse sendSmsResponse = acsClient.getAcsResponse(request);
+
+        return sendSmsResponse;
+    }
+
+    public static Map<String, String> map = new HashMap();
+
+    public synchronized static String getRandomNumCode(String phonenumber, int number) {
+        StringBuilder codeNum = new StringBuilder();
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Random random = new Random();
+        for (int i = 0; i < number; i++) {
+            int next = random.nextInt(10000);//目的是产生足够随机的数，避免产生的数字重复率高的问题
+            codeNum.append(numbers[next % 10]);
+        }
+        System.out.println("---生成验证码-----");
+        System.out.println(codeNum);
+        if (map.size() > 1000) {
+            map.clear();
+        }
+        map.put(phonenumber, codeNum.toString());
+        return codeNum.toString();
     }
 
     public static boolean checkCode(String phoneNumber, String verifyCode) {
-        return true;
+        String verifyCode2 = map.get(phoneNumber);
+        return verifyCode2 != null && verifyCode2.equals(verifyCode);
+//        return true;
     }
-
 //    @RequestMapping("/verifyschool")
 //    public String verifyschool(ModelMap map, School school) {
 //        // 加入一个属性，用来在模板中读取
